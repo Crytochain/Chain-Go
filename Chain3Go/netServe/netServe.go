c731afe41d5ac85e944942d7354314a02610351a
liberumChain <59084371+liberumChain@users.noreply.github.com>
2020-01-13 01:41:13 +0800

    Delete netServe.go

--- Chain3Go/netServe/netServe.go
+++
@@ -1,7 +0,0 @@
-// netServe project netServe.go
-package netServe
-
-type ProviderInterface interface {
-	SendRequest(relust interface{}, method string, params interface{}, netNumber int) error
-	Close() error
-}
