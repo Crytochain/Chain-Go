07cb48d4a056a27e233abe64e5ba88b61c9d69f0
liberumChain <59084371+liberumChain@users.noreply.github.com>
2020-04-29 14:21:55 +0800

    Add files via upload

--- derive_sha.go
+++ derive_sha.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the LBR-core library. If not, see <http://www.gnu.org/licenses/>.
 
-package transaction
+package types
 
 import (
 	"bytes"
